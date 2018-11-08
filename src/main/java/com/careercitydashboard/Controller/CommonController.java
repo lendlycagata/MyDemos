77180b57314f1315a0070731677c9520b57ebb1c
Amelito Estrada <amelito.estrada@telusinternational.com>
2018-11-09 02:50:34 +0800

    Upload Image

--- src/main/java/com/careercitydashboard/Controller/CommonController.java
+++ src/main/java/com/careercitydashboard/Controller/CommonController.java
@@ -1,7 +1,5 @@
 package com.careercitydashboard.Controller;
 
-
-
 import java.io.IOException;
 import java.util.List;
 
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.careercitydashboard.Model.Account;
 import com.careercitydashboard.Model.AccountAnswers;
@@ -24,123 +23,133 @@ import com.careercitydashboard.Service.AnswerService;
 import com.careercitydashboard.Service.PositionAnswerService;
 import com.careercitydashboard.Service.PositionService;
 import com.careercitydashboard.Service.QuestionsService;
-
-
-
-
+import com.careercitydashboard.util.UploadUtility;
 
 @Controller
 public class CommonController {
-	
+
 	@Autowired
 	private AccountService accountService;
-	
+
 	@Autowired
 	private PositionService positionService;
-	
+
 	@Autowired
 	private QuestionsService questionService;
-	
-	@Autowired 
+
+	@Autowired
 	private AccountAnswersService accountAnswersService;
-	
+
 	@Autowired
 	private AnswerService answerService;
-	
+
 	@Autowired
 	private PositionAnswerService positionAnswerService;
-	
+
 	@RequestMapping("/")
-	public String index(Model model){
+	public String index(Model model) {
 		model.addAttribute("welcome", "Career City Admin Dashboard");
 		return "redirect:/login";
 	}
-	
-	   
-	
-	@RequestMapping(value="/listaccount", method=RequestMethod.GET)
-	
-	public String listofallAccounts(Model model) throws IOException{
-		List<Account> listofallaccounts= this.accountService.getallAccounts();
-		
+
+	@RequestMapping(value = "/listaccount", method = RequestMethod.GET)
+
+	public String listofallAccounts(Model model) throws IOException {
+		List<Account> listofallaccounts = this.accountService.getallAccounts();
+
 		model.addAttribute("allaccounts", listofallaccounts);
-		
+
 		return "accounttable";
 	}
-	@RequestMapping(value="/addaccount", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/addaccount", method = RequestMethod.POST)
 	public String addAccount(Account account) {
 		this.accountService.saveAccount(account);
 		return "redirect:/listaccount";
 	}
-	@RequestMapping(value="/updateaccount" , method=RequestMethod.POST)
-	public String updateAccount(Account account) {
+
+	@RequestMapping(value = "/updateaccount", method = RequestMethod.POST)
+	public String updateAccount(Account account, @RequestParam("file") MultipartFile file) {
+
+		// For uploading the image to repository
+		UploadUtility upload = new UploadUtility();
+		if (!file.isEmpty())
+			upload.singleFileUpload(file);
+
 		this.accountService.updateAccount(account);
 		return "redirect:/listaccount";
 	}
-	
 
-	@RequestMapping(value="/accountmaps/{id}", method=RequestMethod.GET)
-	public String getaccountmapbyId(@PathVariable ("id")Integer ACCOUNT_ID, Model model) {
+	@RequestMapping(value = "/accountmaps/{id}", method = RequestMethod.GET)
+	public String getaccountmapbyId(@PathVariable("id") Integer ACCOUNT_ID, Model model) {
 		System.out.println(ACCOUNT_ID);
 		Account getmapdetails = this.accountService.getAccountMapping(ACCOUNT_ID);
-		System.out.println( getmapdetails.toString() );
-		model.addAttribute("mapdetails", getmapdetails );
-		
-		return "accountmap" ;
+		System.out.println(getmapdetails.toString());
+		model.addAttribute("mapdetails", getmapdetails);
+
+		return "accountmap";
 	}
-	@RequestMapping(value="/addaccountanswers", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/addaccountanswers", method = RequestMethod.POST)
 	public String addAccountMapping(AccountAnswers accountAnswers) {
 		this.accountAnswersService.addAccountAnswers(accountAnswers);
 		System.out.println(accountAnswers.toString());
 		return "redirect:/accountmaps/" + accountAnswers.getACCOUNT_ID();
 	}
-	
-	@RequestMapping(value="/deleteaccountmap" , method=RequestMethod.POST)
-	public String deleteAccountMapping(@RequestParam  Integer QUESTION_ANSWER_ID , @RequestParam Integer ACCOUNT_ID  ) {
+
+	@RequestMapping(value = "/deleteaccountmap", method = RequestMethod.POST)
+	public String deleteAccountMapping(@RequestParam Integer QUESTION_ANSWER_ID, @RequestParam Integer ACCOUNT_ID) {
 		System.out.println(QUESTION_ANSWER_ID);
 		this.accountAnswersService.deleteMapping(QUESTION_ANSWER_ID);
-		 return "redirect:/accountmaps/" + ACCOUNT_ID; 
+		return "redirect:/accountmaps/" + ACCOUNT_ID;
 	}
-	
-	@RequestMapping(value="/listposition" , method=RequestMethod.GET)
-	public String listofallPositions(Model model) throws IOException{
-		List<Position> listofallpositions= this.positionService.getallPositions();
+
+	@RequestMapping(value = "/listposition", method = RequestMethod.GET)
+	public String listofallPositions(Model model) throws IOException {
+		List<Position> listofallpositions = this.positionService.getallPositions();
 		model.addAttribute("allpositions", listofallpositions);
 		return "positiontable";
 	}
-	@RequestMapping(value="/updateposition", method=RequestMethod.POST)
-	public String updatePosition(Position position) {
-		 this.positionService.updatePosition(position);		
-		 return "redirect:/listposition";
+
+	@RequestMapping(value = "/updateposition", method = RequestMethod.POST)
+	public String updatePosition(Position position, @RequestParam("file") MultipartFile file ) {
+		// For uploading the image to repository
+		UploadUtility upload = new UploadUtility();
+		if (!file.isEmpty())
+			upload.singleFileUpload(file);
+		this.positionService.updatePosition(position);
+		return "redirect:/listposition";
 	}
-	@RequestMapping(value="/addposition", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/addposition", method = RequestMethod.POST)
 	public String addPosition(Position position) {
 		this.positionService.savePosition(position);
 		return "redirect:/listposition";
 	}
-	@RequestMapping(value="/positionmaps/{id}", method=RequestMethod.GET)
-	public String getPositionMapById(@PathVariable ("id") Integer POSITION_ID, Model model) {
-//		System.out.println(POSITION_ID);
+
+	@RequestMapping(value = "/positionmaps/{id}", method = RequestMethod.GET)
+	public String getPositionMapById(@PathVariable("id") Integer POSITION_ID, Model model) {
+		// System.out.println(POSITION_ID);
 		Position getPositionMapDetails = this.positionService.getPositionMapping(POSITION_ID);
-//		System.out.println(getPositionMapDetails.toString());
-		model.addAttribute("positionmaps",getPositionMapDetails );
-		
+		// System.out.println(getPositionMapDetails.toString());
+		model.addAttribute("positionmaps", getPositionMapDetails);
+
 		return "positionmaps";
-		
+
 	}
-	
-	@RequestMapping(value="/addpositionanswers", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/addpositionanswers", method = RequestMethod.POST)
 	public String addPositionMapping(PositionAnswers positionAnswers) {
 		System.out.println(positionAnswers);
 		this.positionAnswerService.addPositionAnswers(positionAnswers);
-		
+
 		return "redirect:/positionmaps/" + positionAnswers.getPOSITION_ID();
 	}
-	
-	@RequestMapping(value="/deletepositionmap" , method=RequestMethod.POST)
-	public String deletePositionMapping(@RequestParam  Integer QUESTION_ANSWER_ID , @RequestParam Integer POSITION_ID) {
+
+	@RequestMapping(value = "/deletepositionmap", method = RequestMethod.POST)
+	public String deletePositionMapping(@RequestParam Integer QUESTION_ANSWER_ID, @RequestParam Integer POSITION_ID) {
 		this.positionAnswerService.deletePositionMapping(QUESTION_ANSWER_ID);
-		return "redirect:/positionmaps/" + POSITION_ID; 
+		return "redirect:/positionmaps/" + POSITION_ID;
 	}
-	
+
 }
---
+++ src/main/java/com/careercitydashboard/util/UploadUtility.java
@@ -0,0 +1,40 @@
+package com.careercitydashboard.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+public class UploadUtility {
+	/* localpath */
+	private static String UPLOADER_FOLDER = "C://things//";
+
+	/* serverpath-dev */
+	/*
+	 * private static String UPLOADER_FOLDER = "//opt/tomcat/webapps/ImageRepo//";
+	 */
+
+	public String singleFileUpload(MultipartFile file) {
+		String url = "";
+		
+//		if (file.isEmpty()) {
+//			redirectAttributes.addFlashAttribute("warning", "Please Select a file to upload");
+//			return "Please Select a file to upload";
+//		}
+		
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOADER_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+			url = "https://35.185.222.6/ImageRepo/" + file.getOriginalFilename();
+			return url;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+}
